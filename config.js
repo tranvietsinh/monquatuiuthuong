b21f4639bcc6e3d4f029adfe6f76835fc45edd89
Lê Hữu Hoàng Long <63137653+longlee2000@users.noreply.github.com>
2021-06-03 18:57:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Dỗi à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao dỗi cứ đi nhầm đường
+    Lạc vào tim Long lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnIntro: 'haha',
+    title: 'Phải chăng dỗi đã yêu long ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng dỗi đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Long <33',
+    btnNo: 'Không, Long à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Long <3',
+    btnReply: 'Gửi cho Long <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Long biết mà 🥰. Yêu dỗi nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h vô chơi game cùng Long nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/longlee2000' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
